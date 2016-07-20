2e84b0a63b46d82f5de1934cb76ab27cbd78a91b
Schueltke Andre <aschueltke@andres-macbook-pro-7.fritz.box>
2016-07-20 15:20:35 +0200

    Initial Commit

---
+++ MyOwnBombApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOwnBombApp
+//
+//  Created by Andre Schueltke on 20.07.16.
+//  Copyright © 2016 Andre Schültke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
