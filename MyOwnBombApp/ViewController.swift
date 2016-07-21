0bdd264a9e7825f00cff46fe1c97c060776969b4
Andre Schueltke <aschueltke@Andres-MacBook-Pro-7.fritz.box>
2016-07-21 21:24:32 +0200

    1stCommit to GitHub

--- MyOwnBombApp/ViewController.swift
+++ MyOwnBombApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var HideBlueBombBtn: UIButton!
+    @IBOutlet weak var HideRedBombBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        if blueBomb.hidden == true {
+            blueBomb.hidden = false
+        }else {
+            blueBomb.hidden = true
+        }
+    }
+    
+    @IBAction func hideRedBomb(sender: AnyObject) {
+               
+        if redBomb.hidden == true {
+            redBomb.hidden = false
+        }else {
+            redBomb.hidden = true
+        }
+    }
+    
+    
 
 }
 
